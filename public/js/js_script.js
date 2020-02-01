3395fd2b8c2cc4ac39538fc43f352090079dd576
Nicke Löfwenberg <nicke@lofwenberg.com>
2020-02-01 17:07:34 +0100

    Partial commit, still WIP

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -17,29 +17,36 @@ class MenuItem {
     getCalories() {
         return this.calories;
     }
+
+    getNameAndCalories() {
+       return this.itemName + " : " + this.calories + " kcal";
+    }
 }
 
 // --- Main block for code running --- //
 //Creates our five burgers
-let burgerBig = new MenuItem("Excessively Unhealthy Burger", "./img/excessoburger.jpg", 50000, ["Gluten", "Beef", "Everything else"]);
-let burgerSquare = new MenuItem("Squarely Burger", "./img/sqr_borgar.jpg", 2000, ["Beef", "Squares"]);
-let burgerSquareAlternate = new MenuItem("Squarely Burger (Veggie)", "./img/sqr_borgar.jpg", 1400, ["Squares", "Soy"]);
-let burgerTiny = new MenuItem("Teeny Tiny Borgar", "./img/tiny_borgar.jpg", 50, ["Gluten"]);
-let burgerTinyAlternate = new MenuItem("Teeny Tiny Borgar (Gluten Free)", "./img/tiny_borgar.jpg", 62, []);
+let menu = [
+    new MenuItem("Excessively Unhealthy Burger", "./img/excessoburger.jpg", 50000, ["Gluten", "Beef", "Everything else"]),
+    new MenuItem("Squarely Burger", "./img/sqr_borgar.jpg", 2000, ["Beef", "Squares"]),
+    new MenuItem("Squarely Burger (Veggie)", "./img/sqr_borgar.jpg", 1400, ["Squares", "Soy"]),
+    new MenuItem("Teeny Tiny Borgar", "./img/tiny_borgar.jpg", 50, ["Gluten"]),
+    new MenuItem("Teeny Tiny Borgar (Gluten Free)", "./img/tiny_borgar.jpg", 62, [])
+];
 
 //Adds our five burgers to our burger-selection element
-let burgerBigPara = document.createElement("p");
-burgerBigPara.append(burgerBig.getName() + " : " + burgerBig.getCalories());
-document.getElementById("burger-selection").append(burgerBigPara);
-let burgerSquarePara = document.createElement("p");
-burgerSquarePara.append(burgerSquare.getName() + " : " + burgerSquare.getCalories());
-document.getElementById("burger-selection").append(burgerSquarePara);
-let burgerSquareAlternatePara = document.createElement("p");
-burgerSquareAlternatePara.append(burgerSquareAlternate.getName() + " : " +  burgerSquareAlternate.getCalories());
-document.getElementById("burger-selection").append(burgerSquareAlternatePara);
-let burgerTinyPara = document.createElement("p");
-burgerTinyPara.append(burgerTiny.getName() + " : " + burgerTiny.getCalories());
-document.getElementById("burger-selection").append(burgerTinyPara);
-let burgerTinyAlternatePara = document.createElement("p");
-burgerTinyAlternatePara.append(burgerTinyAlternate.getName() + " : " + burgerTinyAlternate.getCalories());
-document.getElementById("burger-selection").append(burgerTinyAlternatePara);
+for (burger in menu) {
+    let newPara = document.createElement("p");
+    newPara.append(burger.getNameAndCalories());
+
+    if (burger.allergyList.length > 0) {
+        let allergenUnorderedList = document.createElement("ul");
+        for (allergen in burger.allergyList) {
+            let newIndex = document.createElement("li");
+            newIndex.append("Contains " + allergen);
+            allergenUnorderedList.append(newIndex);
+        }
+        newPara.append(allergenUnorderedList);
+    }
+    
+    document.getElementById("burger-selection").append(newPara);
+}
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,10 +1,6 @@
 const vm = new Vue({
     el: '#burger-selection',
     data: {
-      bBurg : burgerBig,
-      sqBurg : burgerSquare,
-      sqaBurg : burgerSquareAlternate,
-      tBurg : burgerTiny,
-      taBurg : burgerTinyAlternate
+        menu : menu
     }
-  })
+})
